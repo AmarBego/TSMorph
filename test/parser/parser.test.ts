e9b4a4183e5b99bcf9d88d773da87add430c0fc5
Amar Begovic <amar.begovic123@hotmail.com>
2024-09-12 13:35:57 +0200

    parser update

--- test/parser/parser.test.ts
+++ test/parser/parser.test.ts
@@ -58,4 +58,53 @@ describe('Parser', () => {
         const parser = new Parser(tokens);
         expect(() => parser.parse()).to.throw(ParseError, 'Expected expression.');
     });
+
+    it('parses multiple statements', () => {
+        const input = '1 + 2; 3 * 4;';
+        const tokens = lexer(input);
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+
+        expect(ast).to.deep.equal(
+            new ASTNode('Program', [
+                new ASTNode('ExpressionStatement', [
+                    new ASTNode('Addition', [
+                        new ASTNode('NumberLiteral', [new ASTNode('1')]),
+                        new ASTNode('NumberLiteral', [new ASTNode('2')])
+                    ])
+                ]),
+                new ASTNode('ExpressionStatement', [
+                    new ASTNode('Multiplication', [
+                        new ASTNode('NumberLiteral', [new ASTNode('3')]),
+                        new ASTNode('NumberLiteral', [new ASTNode('4')])
+                    ])
+                ])
+            ])
+        );
+    });
+
+    it('handles division', () => {
+        const input = '10 / 2;';
+        const tokens = lexer(input);
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+
+        expect(ast).to.deep.equal(
+            new ASTNode('Program', [
+                new ASTNode('ExpressionStatement', [
+                    new ASTNode('Division', [
+                        new ASTNode('NumberLiteral', [new ASTNode('10')]),
+                        new ASTNode('NumberLiteral', [new ASTNode('2')])
+                    ])
+                ])
+            ])
+        );
+    });
+
+    it('throws error on missing semicolon', () => {
+        const input = '2 + 3';
+        const tokens = lexer(input);
+        const parser = new Parser(tokens);
+        expect(() => parser.parse()).to.throw(ParseError, "Expected ';' after expression.");
+    });
 });
